4027712108d761d93d0b2cfbac5a6ac0482732c5
tainan <tainansv@gmail.com>
2015-10-01 15:03:24 -0300

    atualizado

--- Programming Assignment 1 Air Pollution/pollutantmean.R
+++
@@ -1,8 +0,0 @@
-pollutantmean <- function(directory, pollutant, id = 1:332){
-    files <- list.files(directory, full.names = TRUE)
-    data <- data.frame()
-    for(i in id){
-        data <- rbind(data, read.csv(files[i]))
-    }
-    x <- mean(data[[pollutant]], na.rm = TRUE)
-}
